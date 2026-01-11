11fd4b29f644cdcd88307c1e3422cb8d2a0f5050
nongyao2516 <nongyao.so@gmail.com>
2026-01-11 18:27:59 +0700

    update v2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="about">
+  <div class="container">
     <h1>This is an about page</h1>
+    <h1>Nongyao Sornjapo</h1>
     
   </div>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,49 @@
-<template >
-    <div>
-        <h1>Contact to Nongyao Sornjapo</h1>
-        <h3>Tel.038-743-690</h3>
-         <img alt="Vue logo" src="../assets/cat.jpg">
+<template>
+  <div class="container">
+    <h1>Contact to Nongyao Sornjapo</h1>
+    <h3>Tel.038-743-690</h3>
+    <img alt="Vue logo" src="../assets/cat.jpg" />
+    <br /><br /><br />
+
+    <div class="container">
+      <div class="row">
+        <!--column 1 -->
+        <div class="col-sm-6">
+          <img alt="Vue logo" width="90%" src="../assets/sea.jpg" />
+        </div>
+       <!--column 2 -->
+        <div class="col-sm-6 bg-body-tertiary">
+          <div class="mb-3 mt-4">
+            <label for="exampleFormControlInput1" class="form-label"
+              >Email address</label
+            >
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label"
+              >Detail</label
+            >
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+          <div class="mb-3 text-center">
+            <button type="button" class="btn btn-primary">Submit</button>&nbsp;
+            <button type="button" class="btn btn-danger">Cancel</button>
+          </div>
+        </div>
+      </div>
     </div>
+
+
+
+    <br /><br />
+  </div>
 </template>
-
-
-
